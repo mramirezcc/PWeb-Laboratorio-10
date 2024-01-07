c9eb3e855b3af7be31456447bf5e54a8b3299e49
mramirezcc <mramirezcc@unsa.edu.pe>
2024-01-06 23:38:48 -0500

    cgi del segundo ejercicio

---
+++ cgi-bin/peliculas1985.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use DBI;
+
+my $db_name   = 'PWeb10';
+my $db_user   = 'max';
+my $db_pass   = '60782999';
+my $db_host   = 'localhost';
+my $dsn = "DBI:mysql:database=$db_name;host=$db_host";
+my $dbh = DBI->connect($dsn, $db_user, $db_pass, { RaiseError => 1, PrintError => 0 });
+
+my $sql = "SELECT * FROM peliculas WHERE YEAR(fecha_lanzamiento) = 1985";
+my $sth = $dbh->prepare($sql);
+$sth->execute();
+print "Content-type: text/html\n\n";
+print <<HTML
+<html>
+    <head>
+        <title>Películas de 1985</title>
+    </head>
+    <body>
+        <h1>Películas de 1985</h1>
+        <table border='1'>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Fecha de Lanzamiento</th>
+            </tr>
+HTML
+while (my $row = $sth->fetchrow_hashref) {
+    print <<HTML
+        <tr>
+            <td>$row->{id}</td>
+            <td>$row->{titulo}</td>
+            <td>$row->{fecha_lanzamiento}</td>
+        </tr>
+HTML
+}
+print "</table>";
+print "</body></html>";
+$dbh->disconnect;
